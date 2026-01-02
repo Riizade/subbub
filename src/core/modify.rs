f7dee8a5f4aee64ad732a58bbd4ec8194583b32c
Adam James-Liebau Johnson <riizade@gmail.com>
2026-01-01 19:27:04 -0500

    bump version, remove subs when underflowing due to timing shifts

--- src/core/modify.rs
+++ src/core/modify.rs
@@ -2,7 +2,7 @@
 
 use anyhow::Result;
 use scraper::Html;
-use srtlib::Subtitles;
+use srtlib::{Subtitle, Subtitles, Timestamp};
 
 pub fn clean_subtitles(subs: &mut Subtitles) -> Result<()> {
     strip_html(subs)?;
@@ -57,15 +57,43 @@ fn strip_html_string(string: &str) -> String {
     strings.join("")
 }
 
+trait Seconds {
+    fn seconds(&self) -> f32;
+}
+
+impl Seconds for Timestamp {
+    fn seconds(&self) -> f32 {
+        let (hours, minutes, seconds, milliseconds) = self.get();
+        return (hours as u32 * 3600) as f32
+            + (minutes * 60) as f32
+            + seconds as f32
+            + (milliseconds as f32) / 1000.0;
+    }
+}
+
 pub fn shift_seconds(subtitles: &Subtitles, seconds: f32) -> Result<Subtitles> {
-    let mut shifted_subs = subtitles.clone().to_vec();
     let iseconds = seconds as i32;
     let imillis = ((seconds - iseconds as f32) * 1000.0) as i32;
-
-    for subtitle in shifted_subs.iter_mut() {
-        subtitle.add_seconds(iseconds);
-        subtitle.add_milliseconds(imillis);
-    }
+    let shifted_subs: Vec<Subtitle> = subtitles
+        .clone()
+        .to_vec()
+        .iter()
+        .map(|subs| {
+            let sub_stamp = subs.start_time.seconds();
+            // prevent underflowing
+            // if we're subtracting time (e.g., -3 seconds), we want to avoid including any subs that start before the 3 second mark
+            // we multiply by -1.0 instead of using abs() because abs would strip subtitles when adding time (displaying later), which we don't want
+            if sub_stamp < -1.0 * seconds {
+                None
+            } else {
+                let mut shifted_subs = subs.clone();
+                shifted_subs.add_seconds(iseconds);
+                shifted_subs.add_milliseconds(imillis);
+                Some(shifted_subs)
+            }
+        })
+        .flatten()
+        .collect();
 
     Ok(Subtitles::new_from_vec(shifted_subs))
 }
